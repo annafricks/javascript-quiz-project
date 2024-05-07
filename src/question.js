fe435ce5b3cbdfe65cff0edb7b810de9786e3002
annafricks <annafricks33@gmail.com>
2024-05-07 18:25:50 -0400

    day 2 of mini project complete

--- src/question.js
+++ src/question.js
@@ -1,17 +1,21 @@
 class Question {
-    constructor (text, choices, answer, difficulty) {
-        this.text = text
-        this.choices = choices
-        this.answer = answer
-        this.difficulty = difficulty
-    }
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-
-    shuffleChoices(){
-      for (let i = this.choices.length - 1; i > 0 ; i--){
-        const newChoices = Math.floor(Math.random() * (i + 1));
-        [this.choices[i]], this.choices[newChoices] = [this.choices[newChoices], this.choices[i]]
-    
-      }
+  shuffleChoices() {
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      const newChoices = Math.floor(Math.random() * (i + 1));
+      [this.choices[i]],
+        (this.choices[newChoices] = [
+          this.choices[newChoices],
+          this.choices[i],
+        ]);
     }
+  }
 }
+
+
--- src/quiz.js
+++ src/quiz.js
@@ -49,6 +49,7 @@ class Quiz {
     else{
         return true 
     }
-    
    }
+
+   
 }
